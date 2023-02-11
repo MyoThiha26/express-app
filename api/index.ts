a46b77648eb10763265fcaab1765a49e21ab63c3
Myo Thiha <mth2682004@gmail.com>
2023-02-11 10:03:01 +0630

    My First Express Project

---
+++ api/index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+
+const port = 3000;
+const apiUrl = process.env.API_URL;
+app.use(express.static("public"));
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>API</title>
+  </head>
+  <body>
+    <h1>Hello API</h1>
+    <script type="text/javascript">
+        localStorage.setItem('apiUrl', '${apiUrl}');
+        window.location.href = "/";
+    </script>
+  </body>
+</html>
+`;
+
+app.get("/users", (req: Request, res: Response) => {
+  res.send({ name: "Myo Thiha", password: "jfoafo" });
+});
+
+app.get("/api", (req: Request, res: Response) => {
+  res.send(html);
+});
+
+app.listen(port, () => {
+  console.log("Server Started Listening : ", port);
+});
